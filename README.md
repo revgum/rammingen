c2144b26e499c27285ab040e19e073580120d523
Josh Gum <josh.gum@oregonstate.edu>
2016-03-12 13:12:15 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# rammingen
+
+A Clojure library designed to work with BagIt files.
+
+## Usage
+
+FIXME
+
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject rammingen "0.0.1"
+  :description "Tool for working with and making BagIt files."
+  :url "https://github.com/revgum/rammingen"
+  :license {:name "MIT"
+            :url "http://mit-license.org/"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
